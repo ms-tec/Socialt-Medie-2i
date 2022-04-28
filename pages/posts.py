e1169154fa85650bf092a287ca9a7efd3d737d4a
Maya Saietz <ms@tec.dk>
2022-04-28 15:01:56 +0200

    Remove .contents-div

--- pages/posts.py
+++ pages/posts.py
@@ -19,34 +19,33 @@ def show_posts(posts=[], user=None):
                                                 filename='style.css'))
 
     with doc:
-        with div(id='contents'):
-            menu_items = [
-                ('Forside', '/'),
-                ('Log ud', '/logout'),
-                ('Ny post', '/write'),
-                ('Nyt billede', '/upload'),
-                ('Rediger profil', '/profile')
-            ]
-            show_menu(menu_items)
-            if user is not None:
-                userprofile.user_profile(user)
-            for display_post in posts:
-                with div(cls='post'):
-                    h1(display_post.post.title)
-                    with div(cls='author'):
-                        a(f'af: {display_post.author.username}',
-                          href=f'/u/{quote(display_post.author.username)}',
-                          cls='author_link')
-                    if isinstance(display_post.post, post.TextPost): # text post
-                        with div():
-                            lines = filter(bool, display_post.post.contents.splitlines())
-                            for par in lines:
-                                p(par)
-                    else: # image post
-                        with div():
-                            img(src=app.url_for('static',
-                                                name='static',
-                                                filename=f'images/posts/{display_post.post.image_id}.png'))
+        menu_items = [
+            ('Forside', '/'),
+            ('Log ud', '/logout'),
+            ('Ny post', '/write'),
+            ('Nyt billede', '/upload'),
+            ('Rediger profil', '/profile')
+        ]
+        show_menu(menu_items)
+        if user is not None:
+            userprofile.user_profile(user)
+        for display_post in posts:
+            with div(cls='post'):
+                h1(display_post.post.title)
+                with div(cls='author'):
+                    a(f'af: {display_post.author.username}',
+                        href=f'/u/{quote(display_post.author.username)}',
+                        cls='author_link')
+                if isinstance(display_post.post, post.TextPost): # text post
+                    with div():
+                        lines = filter(bool, display_post.post.contents.splitlines())
+                        for par in lines:
+                            p(par)
+                else: # image post
+                    with div():
+                        img(src=app.url_for('static',
+                                            name='static',
+                                            filename=f'images/posts/{display_post.post.image_id}.png'))
 
     return doc.render()
 
@@ -60,21 +59,20 @@ def create_image_page():
                                                 filename='style.css'))
 
     with doc:
-        with div(id='contents'):
-            menu_items = [
-                ('Forside', '/'),
-                ('Log ud', '/logout'),
-                ('Ny post', '/write'),
-                ('Rediger profil', '/profile')
-            ]
-            show_menu(menu_items)
-            with form(cls='post-form', enctype='multipart/form-data', method='POST', action='/post/image'):
-                with div(cls='post'):
-                    input_(type='text', cls='title_inp',
-                           name='title',
-                           placeholder='Indtast titel...')
-                    input_(type='file', name='image', accept='image/png')
-                input_(type='submit', value='Post', cls='button')
+        menu_items = [
+            ('Forside', '/'),
+            ('Log ud', '/logout'),
+            ('Ny post', '/write'),
+            ('Rediger profil', '/profile')
+        ]
+        show_menu(menu_items)
+        with form(cls='post-form', enctype='multipart/form-data', method='POST', action='/post/image'):
+            with div(cls='post'):
+                input_(type='text', cls='title_inp',
+                        name='title',
+                        placeholder='Indtast titel...')
+                input_(type='file', name='image', accept='image/png')
+            input_(type='submit', value='Post', cls='button')
 
     return doc.render()
 
@@ -88,22 +86,21 @@ def create_page():
                                                 filename='style.css'))
 
     with doc:
-        with div(id='contents'):
-            menu_items = [
-                ('Forside', '/'),
-                ('Log ud', '/logout'),
-                ('Nyt billede', '/upload'),
-                ('Rediger profil', '/profile')
-            ]
-            show_menu(menu_items)
-            with form(cls='post-form', method='POST', action='/post/text'):
-                with div(cls='post'):
-                    input_(type='text', cls='title_inp',
-                           name='title',
-                           placeholder='Indtast titel...')
-                    textarea(cls='contents_inp',
-                             name='contents',
-                             placeholder='Indtast tekst...')
-                input_(type='submit', value='Post', cls='button')
+        menu_items = [
+            ('Forside', '/'),
+            ('Log ud', '/logout'),
+            ('Nyt billede', '/upload'),
+            ('Rediger profil', '/profile')
+        ]
+        show_menu(menu_items)
+        with form(cls='post-form', method='POST', action='/post/text'):
+            with div(cls='post'):
+                input_(type='text', cls='title_inp',
+                        name='title',
+                        placeholder='Indtast titel...')
+                textarea(cls='contents_inp',
+                            name='contents',
+                            placeholder='Indtast tekst...')
+            input_(type='submit', value='Post', cls='button')
 
     return doc.render()
